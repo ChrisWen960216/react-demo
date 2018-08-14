5ce1984be3e155ba677d4602098d6d138fcf8abd
ChrisWen <957638221@qq.com>
2018-08-14 14:05:00 +0800

    Remove all code for this branch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/App';
-import registerServiceWorker from './net/registerServiceWorker';
-import './styles/index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
